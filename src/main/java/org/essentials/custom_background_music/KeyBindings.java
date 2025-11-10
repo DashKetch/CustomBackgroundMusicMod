edb1f402d5a70bcde291ba9ed0a751c7b4f993e9
DashKetch <nateisthegreatgreat@gmail.com>
2025-11-09 18:52:31 -0800

    tried a new patch for the crash using 6-parameter keymapping

--- src/main/java/org/essentials/custom_background_music/KeyBindings.java
+++ src/main/java/org/essentials/custom_background_music/KeyBindings.java
@@ -1,12 +1,19 @@
 package org.essentials.custom_background_music;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.client.settings.IKeyConflictContext;
+import net.neoforged.neoforge.client.settings.KeyConflictContext;
+import net.neoforged.neoforge.client.settings.KeyModifier;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
     public static final KeyMapping OPEN_MUSIC_GUI = new KeyMapping(
-            "key.custom_background_music.open_gui",
+        "key.custom_background_music.open_gui",
+            KeyConflictContext.IN_GAME,
+            KeyModifier.NONE,
+            InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_M,
             "key.categories.custom_background_music"
     );
