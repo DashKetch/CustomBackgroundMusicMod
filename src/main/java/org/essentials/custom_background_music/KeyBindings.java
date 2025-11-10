49d6209a435423cee006de023d3ccb66f0af3cb6
DashKetch <nateisthegreatgreat@gmail.com>
2025-11-09 19:26:06 -0800

    tried a new patch for the crash using 4-parameter keymapping

--- src/main/java/org/essentials/custom_background_music/KeyBindings.java
+++ src/main/java/org/essentials/custom_background_music/KeyBindings.java
@@ -11,8 +11,6 @@ import org.lwjgl.glfw.GLFW;
 public class KeyBindings {
     public static final KeyMapping OPEN_MUSIC_GUI = new KeyMapping(
         "key.custom_background_music.open_gui",
-            KeyConflictContext.IN_GAME,
-            KeyModifier.NONE,
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_M,
             "key.categories.custom_background_music"
