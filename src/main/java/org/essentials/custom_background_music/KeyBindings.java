499dc66b48eb52ddbc68d122e6ba3a888f65c995
DashKetch <nateisthegreatgreat@gmail.com>
2025-11-09 19:34:38 -0800

    tried a more recent patch

--- src/main/java/org/essentials/custom_background_music/KeyBindings.java
+++ src/main/java/org/essentials/custom_background_music/KeyBindings.java
@@ -2,18 +2,14 @@ package org.essentials.custom_background_music;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
-import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
     public static final KeyMapping OPEN_MUSIC_GUI = new KeyMapping(
-        "key.custom_background_music.open_gui",
+            "key.custom_background_music.open_gui",
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_M,
             "key.categories.custom_background_music"
     );
 
-    public static void register(RegisterKeyMappingsEvent event) {
-        event.register(OPEN_MUSIC_GUI);
-    }
 }
